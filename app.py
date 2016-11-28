263db122377a04204b866f1bc1741e599556f324
Alex Wang <alex3763@gmail.com>
2016-11-27 21:39:43 -0500

    some moderator modifications

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ print("Enter password for " + USERNAME)
 PASSWORD = getpass.getpass()
 
 VERBOSE = True
+EMAIL = False
+
+WEAPON = "The weapon is an index card (or paper of similar size) with your target's name written LEGIBLY on the index card (i.e., if the mods can't read it, it doesn't count).  You may carry as many index cards as you like with as many different names as you wish, but must use ONLY the card with your target's name to assassinate them (i.e., you cannot touch a stack of cards to them)."
+IMMUNITY = "The immunity is holding a fruit (bananas count). If your fruit is ambiguous, it doesn't count."
 
 tic = time()
 fin = open("names.txt").read().splitlines()
@@ -35,14 +39,37 @@ for i in range(len(fin)):
 	if i == len(fin)-1:
 		target = 0
 	addr = USERNAME + '@tjhsst.edu'
-	message = ("Hello {},\n\nYour target for assassins is {}.  Good luck!\n\n-AutoModerator\n\nThis is an automated message.").format(fin[i][0],fin[target][0])
+	message = ("Hello {},\n\nYour target for assassins is {}.\n{}\n{}\nGood luck!\n\n-AutoModerator\n\nThis is an automated message.").format(fin[i][0],WEAPON,IMMUNITY,fin[target][0])
 	msg = email.mime.text.MIMEText(message)
 	msg['Subject'] = "Assassins Target"
 	msg['From'] = addr
 	msg['To'] = fin[i][1]
-	s.send_message(msg)
+	if EMAIL:
+		try:
+			s.send_message(msg)
+		except:
+			print("Error sending message to", fin[i][0])
 	if VERBOSE:
 		print("Message sent to", fin[i][0])
+mods = open("mod.txt").read().splitlines()
+for i in mods:
+	addr = USERNAME + '@tjhsst.edu'
+	message = "Hello Moderator,\n\nHere is the assassins targeting list:\n\n"
+	message += ("Total Participants: " + str(len(fin)) + "\n")
+	for j in fin:
+		message += (j[0] + "\n")
+	message += "\n-AutoModerator\n\nThis is an automated message."
+	msg = email.mime.text.MIMEText(message)
+	msg['Subject'] = "Assassins List"
+	msg['From'] = addr
+	msg['To'] = i
+	if EMAIL:
+		try:
+			s.send_message(msg)
+		except:
+			print("Error sending moderator message to", i)
+	if VERBOSE:
+		print("Moderator Message sent to", i)
 
 s.quit()		
 toc = time()
